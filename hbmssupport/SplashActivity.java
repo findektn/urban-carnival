d1858840bad0f622855f884ebc748a3b039b3c34
Finde@aon.at <finde@aon.at>
2018-02-25 01:56:11 +0100

    jkasdhfaksjhf

---
+++ hbmssupport/SplashActivity.java
@@ -0,0 +1,63 @@
+package com.hbms.hbmssupport;
+
+/**
+ * Created by gmorak on 09.09.16.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+public class SplashActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1000;//3000;
+    private final int pBarMax = 2;
+    private int progress = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final ProgressBar pBar = (ProgressBar) findViewById(R.id.progress_bar);
+        pBar.setMax(pBarMax);
+        final Thread pBarThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (progress <= pBarMax) {
+                        pBar.setProgress(progress);
+                        sleep(300);
+                        ++progress;
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+
+        pBarThread.start();
+
+        new Handler().postDelayed(new Runnable() {
+
+         /*
+          * Showing splash screen with a timer. This will be useful when you
+          * want to show case your app logo / company
+          */
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainHBMSActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
+    }
+
+
+}
---
+++ hbmssupport/StartFragment.java
@@ -0,0 +1,29 @@
+package com.hbms.hbmssupport;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gertmorak on 29.11.16.
+ */
+
+public class StartFragment extends Fragment {
+
+
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.empty_fragment, container, false);
+
+        return view;
+
+    }
+
+}
+
+
+
